5b7ffb7bd7c93d0d424b2c3ab9f1ae31a48f395f
A4-TECHNOLOGIE <technicien.sav@a4.fr>
2026-06-04 09:19:00 +0200

    Update custom.ts
    Changement du name space a4_BE_CONVO + logo

--- custom.ts
+++ custom.ts
@@ -5,9 +5,9 @@ enum ButtonChoice { // définition des valeurs possibles pour le choix du BP
     D
 }
 
-//% weight=100 color=#EB8AEB icon="\u26a1"
+//% weight=100 color=#EB8AEB icon="\uf06e"
 //% groups="['Push Buttons', 'Potentiometer', 'Motor', 'Servomotor', 'OLED Screen', 'Light ring']"
-namespace conveyor {
+namespace a4_BE_CONVO {
 
     //%block="On button %button pressed"
     //%group='Push Buttons'
